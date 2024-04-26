9bff79d3fa2521c226f855e137ad90ba4ba7ec4d
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-04-26 18:40:46 +0530

    changes

--- fb-mail-auth/src/Components/Signin.jsx
+++ fb-mail-auth/src/Components/Signin.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
-import "./Signin.css"
+import "./Signin.css";
 const SignIn = ()=>{
     const [email,setEmail]=useState('');
     const[password,setPassword]=useState('');
